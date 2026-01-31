57601d8c364269bc5e34c83c0d3feda0d790d750
kartik428 <emaildeleted444@gmail.com>
2026-01-31 15:56:52 +0530

    image correction

--- src/pages/Impact.jsx
+++ src/pages/Impact.jsx
@@ -74,7 +74,7 @@ const Impact = () => {
 const elements = [
   {
     id: "01",
-    image: "../../src/assets/6keyelementimg/Pbanner.jpeg",
+    image: "../../public/6keyelementimg/Pbanner.jpeg",
     title: "Your banner is prime real estate",
     subtitle: "Don't leave it blank! Use it to showcase:",
     list: [
@@ -87,7 +87,7 @@ const elements = [
   },
   {
     id: "02",
-    image: "../../src/assets/6keyelementimg/Pprofile.jpeg",
+    image: "../../public/6keyelementimg/Pprofile.jpeg",
     title: "Your profile pic should have a clean headshot",
     subtitle: "NOT NECESSARILY FORMAL, JUST GENUINELY YOU",
     list: [
@@ -101,7 +101,7 @@ const elements = [
   },
   {
     id: "03",
-    image: "../../src/assets/6keyelementimg/Aboutimg.jpeg",
+    image: "../../public/6keyelementimg/Aboutimg.jpeg",
     title: "Skip vague words",
     subtitle: "NO 'SEEKING', NO 'ENTHUSIASTS'. SAY WHAT YOU DO.",
     description:
@@ -112,7 +112,7 @@ const elements = [
   },
   {
     id: "04",
-    image: "../../src/assets/6keyelementimg/Featuresimg.jpeg",
+    image: "../../public/6keyelementimg/Featuresimg.jpeg",
     title: "Your About should tell your story",
     subtitle: "SKIP THE BUZZWORDS (WRITE LIKE YOU ACTUALLY TALK!)",
     list: [
@@ -126,7 +126,7 @@ const elements = [
   },
   {
     id: "05",
-    image: "../../src/assets/6keyelementimg/Experienceimg.jpeg",
+    image: "../../public/6keyelementimg/Experienceimg.jpeg",
     title: "Featured posts = your best hits + products",
     description:
       "Add your top posts, portfolio, courses, testimonials, personal website, or even a Google Doc of your best work.",
@@ -141,7 +141,7 @@ const elements = [
   },
   {
     id: "06",
-    image: "../../src/assets/6keyelementimg/experience.jpeg",
+    image: "../../public/6keyelementimg/experience.jpeg",
     title: "Make your experience section results-driven",
     subtitle: "DON'T JUST LIST DUTIES - SHOWCASE IMPACT!",
     description:
@@ -296,12 +296,13 @@ const elements = [
 
 <div className="grid md:grid-cols-3 gap-6 mt-16">
   {[
-    { images: "../../src/assets/realresultsimg/img1.jpeg" },
-    { images: "../../src/assets/realresultsimg/img2.jpeg" },
-    { images: "../../src/assets/realresultsimg/img3.jpeg" },
-    { images: "../../src/assets/realresultsimg/img4.jpeg" },
-    { images: "../../src/assets/realresultsimg/img5.jpeg" },
-    { images: "../../src/assets/realresultsimg/img6.jpeg" },
+    { images: "../../public/realresultsimg/img1.jpeg" },
+    { images: "../../public/realresultsimg/img2.jpeg" },
+    { images: "../../public/realresultsimg/img3.jpeg" },
+    { images: "../../public/realresultsimg/img4.jpeg" },
+    { images: "../../public/realresultsimg/img5.jpeg" },
+    { images: "../../public/realresultsimg/img6.jpeg" },
+   
   ].map((item, i) => (
     <div
       key={i}
